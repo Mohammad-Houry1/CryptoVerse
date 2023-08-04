afe00c014f88fdc2ade940269277a71b15ce4278
Mohammad Houry <houry.k.mohamad@gmail.com>
2023-08-04 14:31:09 +0300

    s

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,52 +1,72 @@
 <template>
-  <div class="bg-gray-100 py-10">
+  <div class="py-10">
     <div class="max-w-3xl mx-auto px-4">
-      <h1 class="text-4xl font-bold text-center mb-8">About Us</h1>
+      <h1 class="text-5xl text-white font-bold text-center mb-8">About Us</h1>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div class="bg-white p-6 rounded-lg shadow-lg">
-          <div class="text-2xl font-semibold mb-4">Our Mission</div>
+        <div class="bg-wh text-white p-6 rounded-lg shadow-lg">
+          <div class="text-2xl font-semibold mb-4">Our Mission:</div>
           <p class="text-gray-700">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac finibus tellus. Mauris
-            vitae risus nec orci scelerisque hendrerit. Cras ut justo vitae odio dictum blandit. In
-            et facilisis sapien.
+            At CryptoVerse, our mission is to provide users with a comprehensive platform for
+            exploring the world of cryptocurrencies. We aim to deliver real-time market data, news,
+            and valuable insights to empower our users to make informed decisions in the crypto
+            market.
           </p>
         </div>
-        <div class="bg-white p-6 rounded-lg shadow-lg">
-          <div class="text-2xl font-semibold mb-4">Our Vision</div>
+        <div class="bg-wh text-white p-6 rounded-lg shadow-lg">
+          <div class="text-2xl font-semibold mb-4">Our Vision:</div>
           <p class="text-gray-700">
-            Phasellus non erat sit amet odio eleifend vulputate. Etiam a quam quis purus viverra
-            iaculis. Proin feugiat interdum sem eu porttitor. Nullam eget tristique mauris, vel
-            vestibulum ex.
+            Our vision is to become a leading global destination for crypto enthusiasts, traders,
+            and investors. We strive to create a user-friendly interface that offers cutting-edge
+            tools and resources to navigate the complexities of the cryptocurrency world seamlessly.
           </p>
         </div>
       </div>
-      <div class="mt-10 text-center">
-        <div class="text-3xl font-bold mb-4">Meet Our Team</div>
-        <div class="flex justify-center">
-          <div class="flex flex-wrap gap-8">
-            <!-- Team Member 1 -->
-            <div class="bg-white p-4 rounded-lg shadow-lg max-w-xs">
-              <div
-                class="w-16 h-16 mx-auto mb-4 bg-gray-200 rounded-full flex items-center justify-center"
-              >
-                <i class="text-gray-600 text-2xl fas fa-user"></i>
-              </div>
-              <div class="text-xl font-semibold mb-2">John Doe</div>
-              <div class="text-gray-700">CEO & Co-founder</div>
-            </div>
+    </div>
 
-            <!-- Team Member 2 -->
-            <div class="bg-white p-4 rounded-lg shadow-lg max-w-xs">
-              <div
-                class="w-16 h-16 mx-auto mb-4 bg-gray-200 rounded-full flex items-center justify-center"
-              >
-                <i class="text-gray-600 text-2xl fas fa-user"></i>
-              </div>
-              <div class="text-xl font-semibold mb-2">Jane Smith</div>
-              <div class="text-gray-700">CTO</div>
-            </div>
-            <!-- Add more team members here -->
+    <div class="text-white py-12">
+      <div class="max-w-3xl mx-auto px-4">
+        <div class="text-5xl text-white font-bold text-center mb-8">Explore the Crypto Market</div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div class="bg-wh text-white p-6 rounded-lg shadow-lg">
+            <div class="text-xl font-semibold mb-4">Real-time Data:</div>
+            <p class="text-gray-700">
+              Stay updated with the latest market data. We provide real-time cryptocurrency prices,
+              market capitalizations, trading volumes, and more. Our data is sourced from reliable
+              exchanges to ensure accuracy and reliability.
+            </p>
           </div>
+          <div class="bg-wh text-white p-6 rounded-lg shadow-lg">
+            <div class="text-xl font-semibold mb-4">Market Analysis:</div>
+            <p class="text-gray-700">
+              Gain valuable insights with our market analysis. Our team of experts closely monitors
+              the crypto market to provide you with in-depth analysis and trends that can help you
+              make better investment decisions.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="max-w-3xl mx-auto px-4">
+      <div class="text-5xl text-white font-bold text-center mb-8">
+        Stay Informed with Crypto News
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div class="bg-wh text-white p-6 rounded-lg shadow-lg">
+          <div class="text-2xl font-semibold mb-4">Latest News Coverage:</div>
+          <p class="text-gray-700">
+            Stay updated with the latest news and developments in the crypto world. Our dedicated
+            team of writers and analysts curate and deliver high-quality news articles, analysis,
+            and interviews from reputable sources across the globe.
+          </p>
+        </div>
+        <div class="bg-wh text-white p-6 rounded-lg shadow-lg">
+          <div class="text-2xl font-semibold mb-4">Educational Resources:</div>
+          <p class="text-gray-700">
+            Enhance your understanding of blockchain technology and cryptocurrencies with our
+            educational resources. We offer beginner-friendly guides, tutorials, and in-depth
+            articles to help you navigate the crypto space confidently.
+          </p>
         </div>
       </div>
     </div>
